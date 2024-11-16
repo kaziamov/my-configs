8b031105c28514028eeb2be352e0b4bf1a4c4096
Ilia Kaziamov <kaziamov@outlook.com>
2024-11-16 11:34:30 +0400

    Update

--- ubuntu20/restore-gui.sh
+++ ubuntu20/restore-gui.sh
@@ -1,6 +1,6 @@
 # https://askubuntu.com/questions/1243134/black-screen-on-startup-ubuntu-20-04
-sudo apt-add-repository ppa:yannubuntu/boot-repair  
-sudo apt update  
-sudo apt install -y boot-repair  
-boot-repair  
-sudp apt install lightdm
+# sudo apt-add-repository ppa:yannubuntu/boot-repair  
+# sudo apt update  
+# sudo apt install -y boot-repair  
+# boot-repair  
+sudo apt install lightdm
