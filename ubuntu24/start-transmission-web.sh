f3a0e81aa6c354a73653fa593c496aa43f9022bb
Ilia Kaziamov <kaziamov@outlook.com>
2024-12-26 18:19:52 +0400

    fast update

--- ubuntu24/start-transmission-web.sh
+++ ubuntu24/start-transmission-web.sh
@@ -1,4 +1,6 @@
 sudo apt-get install transmission-cli transmission-common transmission-daemon
+sudo service transmission-daemon stop
 sudo touch /etc/transmission-daemon/settings.json
 sudo mv settings.json /etc/transmission-daemon/settings.json
-cat /etc/transmission-daemon/settings.json
+sudo cat /etc/transmission-daemon/settings.json
+sudo service transmission-daemon start
