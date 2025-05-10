3f8a5c706c1a4c5c1455955dd8ac560e02517ed1
Ilia Kaziamov <kaziamov@outlook.com>
2025-05-10 12:12:33 +0400

    Update install-node.sh

--- ubuntu20/install-node.sh
+++ ubuntu20/install-node.sh
@@ -1,3 +1,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm install 22
